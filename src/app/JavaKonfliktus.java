350fd60093fd30b167142187e439ecb68d2f138a
ponauerszilvi1@gmail.com <ponauer.szilvia.maria2@diak.szamalk-szalezi.hu>
2026-09-23 15:52:34 +0200

    kever metodus

--- src/app/JavaKonfliktus.java
+++ src/app/JavaKonfliktus.java
@@ -3,7 +3,30 @@ package app;
 public class JavaKonfliktus {
 
     public static void main(String[] args) {
-       
+        kever();
     }
-    
+
+    private static void kever() {
+        String[] uj = new String[22];
+        int index = 1;
+
+        for (int i = 1; i <= 3; i++) {
+
+            if (i != oszlop) {
+
+                for (int j = 0; j < 7; j++) {
+                    uj[index] = pakli[i + j * 3];
+                    index++;
+                }
+            }
+        }
+
+        for (int j = 0; j < 7; j++) {
+            uj[index] = pakli[oszlop + j * 3];
+            index++;
+        }
+
+        pakli = uj;
+    }
+
 }
