4018deb74d5559875552c8a5fc71274e8bad7e81
hrvthlevi <horvathleventeroland@gmail.com>
2026-09-23 15:51:44 +0200

    case 2, case 3 kartyaKitalalo jatek szerint

--- src/app/JavaKonfliktus.java
+++ src/app/JavaKonfliktus.java
@@ -3,7 +3,22 @@ package app;
 public class JavaKonfliktus {
 
     public static void main(String[] args) {
-       
+    
+
+        case 2:
+            for(int i = 1;i<7;i++)  {
+                uj[i+7]=pakli[19-(i-1)*3];
+                uj[i]=pakli[20-(i-1)*3];
+                uj[i+14]=pakli[21-(i-1)*3];  
+            }
+            break;
+        case 3:
+            for (int i = 1; i < 7; i++) {
+                uj[i+7]=pakli[19-(i-1)*3];
+                uj[i+14]=pakli[21-(i-1)*3];
+                uj[i]=pakli[20-(i-1)*3];     
+                }
+                break;
     }
     
 }
